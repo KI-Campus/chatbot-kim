950ca9911c890e0cf8687fe72589d80d34b36b29
Theresa Zobel <theresa.zobel@hpi.de>
2022-10-10 12:32:31 +0200

    Set current course title to None after receiving the achievements (#9)

--- rasa/KI-Campus_en/actions/actions.py
+++ rasa/KI-Campus_en/actions/actions.py
@@ -118,7 +118,10 @@ class ActionGetAchievements(Action):
 			return[SlotSet('current_course_achieved', course_achieved), SlotSet('current_course', currentCourse), SlotSet('current_achievements', currentAchievements)]
 		else:
 			dispatcher.utter_message('I am very sorry! I could not find the course you are looking for. Please try again by telling me the course title.')
-			return[SlotSet('current_course_achieved', course_achieved), SlotSet('current_course', currentCourse), SlotSet('current_achievements', currentAchievements)]
+			return[SlotSet('current_course_achieved', course_achieved), 
+			SlotSet('current_course', currentCourse), 
+			SlotSet('current_achievements', currentAchievements),
+			SlotSet('current_course_title', None)]
 
 class ActionGetCertificate(Action):
 	def name(self) -> Text:
